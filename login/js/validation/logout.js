813a2510e96fa38823b88bc937a3fcc30b4c163b
niyonkurueric <niyonkurufamous@gmail.com>
2022-01-13 20:25:44 +0200

    login varidation and logoutog

--- login/js/validation/logout.js
+++ login/js/validation/logout.js
@@ -1,8 +1,16 @@
 const logout = document.getElementById("logout");
 logout.addEventListener("click", () => {
-    console.log("logout");
     const logout = JSON.parse(localStorage.getItem("userInfo"));
     logout.islogin = false;
     localStorage.setItem("userInfo", JSON.stringify(logout));
+    window.location.replace("http://127.0.0.1:5500/login/login.html");
+})
+
+
+const logout1 = document.getElementById("logout1");
+logout1.addEventListener("click", () => {
+    const logout = JSON.parse(localStorage.getItem("userInfo"));
+    logout1.islogin = false;
+    localStorage.setItem("userInfo", JSON.stringify(logout1));
     window.location.replace("http://127.0.0.1:5500/login/login.html");
 })
